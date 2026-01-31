d8152523b139fd157ec9f37b7f170e38ecd45b28
Ronak1605 <rpat256@aucklanduni.ac.nz>
2026-01-31 20:33:21 +1300

    feat: Added input File
    The Input data class in the file is ussed to store the content from the input. A data class wwas sued instead of a normal class, as it is intended to store information/data.

---
+++ ingestion/models/input.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+from datetime import datetime
+from ingestion.models.input_source import InputSource
+
+
+@dataclass
+class Input:
+    """
+    Representation for all ingested content.
+    """
+    id: str
+    text: str
+    source: InputSource
+    created_at: Optional[datetime]
+    metadata: Dict[str, str]
